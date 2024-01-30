28102b150edeab305b2d95f42de8a993fbc54626
Jans-gev <67145788+Jans-gev@users.noreply.github.com>
2024-01-30 05:34:08 -0800

    slider.js

--- slider.js
+++ slider.js
@@ -59,3 +59,15 @@ function AutoPlay() {
 }
 
 AutoPlay();
+
+const togglebtn=document.querySelector('.toggle_btn')
+const togglebtnIcon=document.querySelector('.toggle_btn i')
+const dropdownmenu=document.querySelector('.dropdown_menu')
+
+togglebtn.onclick=function(){
+  dropdownmenu.classList.toggle('open')
+  const isOpen=dropdownmenu.classList.contains('open')
+  togglebtnIcon.classList=isOpen
+   ?'fa-solid fa-xmark'
+   :'fa-solid fa-bars'
+}
